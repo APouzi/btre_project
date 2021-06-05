a45f642443d36d8cdc31b5ec92ce133e4b364511
Alexandre Pouzikov <alexandrepouzikov@gmail.com>
2021-06-05 01:43:55 -0700

    video checkpoint again :)

--- listings/admin.py
+++ listings/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
+from .models import Listing
 
-# Register your models here.
+#Create Superuser & Register Models With Admin -  3:45 so this is basically where we can customize admin stuff for the for the listings app and I want to register register the listing for the admin area. 4:00 So firstly, we want to import Listing from .models. and I'm using listing because in my listings/models.py that's what I called it. So whatever you have here is what you want to have in listings/models.py
+
+#Create Superuser & Register Models With Admin -  4:27 and to register the model, we want to do a admin.site.register(Listing), 4:37 then if we go back to admin area on the site. We should see the listings model. so if I click on listings, we don't have any at the moment, but they would be listed here and we can add them. Notice that we have all of the fields that we added, even realtor, so we can choose a realtor title. From 4:37 to 6:37 he talks about the different fields we inputted in there and how Django gives us alot of features. 
+
+#Create Superuser & Register Models With Admin -  6:22 So I think what I want to do now before we start adding data is also add the realtors in the in the admin area.So to do that, we're going to do the same thing. (6:39 copy the import and one line of code we wrote to paste into realtors/admin)
+
+admin.site.register(Listing) 
--- realtors/admin.py
+++ realtors/admin.py
@@ -1,3 +1,5 @@
 from django.contrib import admin
+from .models import Realtor
+#Create Superuser & Register Models With Admin - 6:54 paste code here. Then the admin area shows that we have Realtor functionality after we refresh the page. So now that means we have realtors and listings as part of the admin area. Later we will customize the admin area, we can specifiy how data including the listings is shown. We have alot of control in this regard
 
-# Register your models here.
+admin.site.register(Realtor) 
